8dcae3f205c3ca12080f979da38908e02c498a13
Estevão Boaventura <85800725+Ventura08@users.noreply.github.com>
2022-10-30 23:43:09 -0300

    seting base routes for project (#2)

--- hot-dog-ui/src/App.tsx
+++ hot-dog-ui/src/App.tsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import React, { ReactElement } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import WhatWeDo from './pages/WhatWeDo/WhatWeDo'
+import WhereAreWe from './pages/WhereAreWe/WhereAreWe'
+import WhoDo from './pages/WhoDo/WhoDo'
 
-function App() {
-  return <div className="App"></div>;
+const App = (): ReactElement => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="who-do" element={<WhoDo />} />
+        <Route path="what-we-do" element={<WhatWeDo />} />
+        <Route path="where-are-we" element={<WhereAreWe />} />
+      </Routes>
+
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ hot-dog-ui/src/pages/WhatWeDo/WhatWeDo.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react'
+
+const WhatWeDo = (): ReactElement => {
+  return (
+    <div>
+      <h1>WhatWeDoPage</h1>
+    </div>
+  )
+}
+
+export default WhatWeDo
